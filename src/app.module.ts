7c2733556952a536f7514dd96ca32f3896b4a92f
Allan <ariosmaia@yahoo.com.br>
2020-03-05 15:00:03 -0300

    Introdução e pacotes

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ReportsModule } from './reports/reports.module';
+
+@Module({
+  imports: [
+		GraphQLModule.forRoot({
+			//debug: false,
+			//playground: false,
+			installSubscriptionHandlers: true,
+			autoSchemaFile: 'schema.gql',
+		}),
+		ReportsModule
+	],
+})
+export class AppModule {}
